2478a21a484f3f421f2c583ba933e5d166b84602
Alex Nielson <alexnielson89@outlook.com>
2021-11-01 17:58:25 -0600

    oh

--- src/models/prove03-model.js
+++ src/models/prove03-model.js
@@ -2,7 +2,7 @@
 // const path = require('path');
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const animalSchema = mongoose.Schema({
     name: {
