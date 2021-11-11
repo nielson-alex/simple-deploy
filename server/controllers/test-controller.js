bafb7899393d43ce0583fdce881dbe89a5a2405a
Alex Nielson <alexnielson89@outlook.com>
2021-11-11 14:59:26 -0700

    Postgres didn't work after pushing to heroku

--- server/controllers/test-controller.js
+++ server/controllers/test-controller.js
@@ -1,37 +1,33 @@
-const Animal = require('../../src/models/prove03-model');
-// const host = "10.0.0.249";
-// const database = "postgres";
-// const schema = "test";
-const host = "pg.eqxnutra.com";
-const database = "equinox";
-const schema = "sandbox";
-const user = "postgres";
-const password = "";
-const pgPort = 5432;
-const PgPool = require("pg").Pool;
-
+const DATABASE_URL = "postgresql://postgres:password@pg.eqxnutra.com:5432/equinox";
 function setUpPool() {
     const Pool = require("pg").Pool;
-    const pool = new Pool({
-        // connectionString: process.env.DATABASE_URL || `postgresql://postgres:<>@pg.eqxnutra.com:5432/equinox`,
-        connectionString: process.env.DATABASE_URL || `postgresql://${user}:<${password}>@${host}:${pgPort}/${database}`,
-        ssl: process.env.DATABASE_URL ? true : false
-    })
-    // const pool = new Pool({
-    //     user: "postgres",
-    //     host: host,
-    //     database: database,
-    //     password: "",
-    //     port: pgPort,
-    // });
+    let pool;
+
+
+    if (process.env.DATABASE_URL) {
+        pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: true
+        })
+    } else {
+        pool = new Pool({
+            user: "postgres",
+            host: "pg.eqxnutra.com",
+            database: "equinox",
+            password: "",
+            port: 5432,
+        });
+    }
 
     return pool;
 }
 
 exports.getAnimals = function (request, response, next) {
     const query = `
-        SELECT  *
-        FROM    equivoice.users
+        SELECT DISTINCT location_group_id,
+                        location_group
+        FROM			oms.warehouse_recode
+        ORDER BY		location_group ASC
     `;
 
     new Promise(function (resolve, reject) {
@@ -41,23 +37,11 @@ exports.getAnimals = function (request, response, next) {
                     reject(error);
                 }
 
-                if (results) {
-                    if (results.rows) {
-                        response.send(results.rows);
-                    } else {
-                        response.send({ "oh": "no" });
-                    }
-                } else {
-                    response.send({ "oh": "no" });
+                if (results.rows) {
+                    response.send(results.rows);
                 }
             });
     });
-
-
-
-
-
-
     // const page = req.query.page;
     // console.log('page', page);
     // let totalItems;
